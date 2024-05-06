e1df647b51bdcebe8a65024a11b90279d4fc8bdf
Vladimir <vova.demenchuck@gmail.com>
2024-05-06 21:05:14 +0300

    First commit

---
+++ very-simple-calc.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+int main() {
+	int firstNum = 0;
+	int secondNum = 0;
+	char operator_ = '+';
+
+	std::cout << "Enter your first number" << std::endl;
+	std::cin >> firstNum;
+	std::cout << "Enter your second number" << std::endl;
+	std::cin >> secondNum;
+	std::cout << "Enter operator" << std::endl << "Example: " << std::endl << "* for multiplication" << std::endl << "+ for addition" << std::endl << "- for subtraction" << std::endl << "/ for division" << std::endl;
+	std::cin >> operator_;
+
+	switch (operator_){
+	case '+':
+		std::cout << firstNum + secondNum;
+		break;
+	case '-':
+		std::cout << firstNum - secondNum;
+		break;
+	case '*':
+		std::cout << firstNum * secondNum;
+		break;
+	case '/':
+		if (secondNum != 0 && firstNum != 0) {
+			std::cout << firstNum / secondNum;
+		}
+		else {
+			std::cout << "division by zero is forbidden";
+		}
+		break;
+	default:
+		std::cout << "Incorrect input";
+	}
+	return 0;
+}
